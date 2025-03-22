96b77d6b4c8282127eebc43abf99883c5377f1a0
navya <navyareddythummala@gmail.com>
2025-03-22 11:34:07 +0530

    Enhance login with authentication

--- login.py
+++ login.py
@@ -2,3 +2,9 @@ def login():
     print("use login functionality")
 
 login()
+if username== "admin"and password== "admin123":
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
+
+login("admin","admin123")
